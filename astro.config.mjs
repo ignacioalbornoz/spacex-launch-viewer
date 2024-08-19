69e98e7366f473d2455b0faedd0091ec17fab457
ignacioalbornoz <52728344+ignacioalbornoz@users.noreply.github.com>
2024-08-19 03:39:06 -0400

    vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-import vercel from "@astrojs/vercel"; // Import the 'vercel' module
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  adapter: vercel(),
   output: "hybrid",
+  adapter: vercel(),
 });
